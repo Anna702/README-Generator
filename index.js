6fd6da977b07faf4b282a507f30df68f558efe55
Anna Chernova <annanvks@gmail.com>
2024-02-05 13:23:08 +0000

    added a function to validate user input

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ inquirer
       type: "input",
       name: "title",
       message: "What is the title of your project?",
+      validate: validInput,
     },
     {
       type: "confirm",
@@ -18,14 +19,28 @@ inquirer
     {
       type: "input",
       name: "description",
-      message: "Can you provide a short description of your project?",
+      message: "Provide a short description of your project",
       when: (answers) => answers.hasDescription,
+      validate: validInput,
     },
     {
       type: "input",
       name: "whyCreated",
       message: "Why did you create this project?",
       when: (answers) => answers.hasDescription,
+      validate: validInput,
+    },
+    {
+      type: "confirm",
+      name: "tableOfContents",
+      message: "Do you want to include a Table of Contents?",
+      default: true,
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for your project:",
+      choices: ["MIT", "BSD", "Apache 2.0", "ISC", "GPLv3", "None"],
     },
   ])
   .then((resp) => {
@@ -37,9 +52,19 @@ inquirer
       `${resp.title.replace(/\s+/g, "-").toLowerCase()}_README.md`
     );
 
+    //creating a new readme file
     fs.writeFile(fileName, JSON.stringify(resp), (err) => {
       err
         ? console.log("Error writing the README file:", err)
         : console.log(`Your README file '${fileName}' successfully created.`);
     });
   });
+
+//function checks if the input is not empty
+function validInput(input) {
+  return input.trim() !== "" || "Please answer the question with any data.";
+}
+
+// function getALicense(){
+
+// }
